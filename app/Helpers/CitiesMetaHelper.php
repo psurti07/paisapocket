995914e84fb1ef6dd7af453cf327b0cdc8233d18
vw-uday <verloop.dev4@gmail.com>
2026-03-18 18:22:03 +0530

    code add

---
+++ app/Helpers/CitiesMetaHelper.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+
+/* Home meta tags */
+if(!function_exists('surat')){
+    function surat(){
+        $meta = [
+            "title" => "Personal Loan in Surat – Instant Approval | Paisapocket",
+            "description" => "Apply for a personal loan in Surat with Paisapocket. Get quick approval, minimal documents & best interest rates from top NBFCs. Check eligibility now!",
+            "keywords" =>  "personal loan in Surat, Surat personal loan, instant personal loan Surat, personal loan NBFC Surat, low interest loan in Surat, Paisapocket loan Surat"
+        ];
+        return $meta;
+    }
+}
